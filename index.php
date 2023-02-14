f9fbac49e951946bb62605650a5221aa1e0787b5
MaryJane <mj.louise007@gmail.com>
2023-02-14 23:55:21 +0800

    added new navbar

--- index.php
+++ index.php
@@ -1,72 +1,50 @@
 <!DOCTYPE html>
-<!-- Created By CodingNepal -->
-<html lang="en" dir="ltr">
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <title>Portfolio</title>
-    <link rel="stylesheet" href="css/style.css">
-    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>Home</title>
+    <link rel="stylesheet" href="css/style.css" />
 </head>
+
 <body>
-<div class="btn">
-    <span class="fas fa-bars"></span>
-</div>
-<nav class="sidebar">
-    <div class="text">
-        Portfolio<br>Diaz, Jericho V.
-    </div>
-    <ul>
-        <li class="active"><a href="ELECT2_SWM1.php">Dashboard</a></li>
-        <li>
-            <a href="#" class="feat-btn">Seatworks
-                <span class="fas fa-caret-down first"></span>
-            </a>
-            <ul class="feat-show">
-                <li><a href="ELECT2_SWM1.php">Seatwork 1</a></li>
-                <li><a href="ELECT2_SWM2_1.php">Seatwork 2.1</a></li>
-                <li><a href="ELECT2_SWM2_2.php">Seatwork 2.2</a></li>
-            </ul>
-        </li>
-        <li>
-            <a href="#" class="serv-btn">Services
-                <span class="fas fa-caret-down second"></span>
-            </a>
-            <ul class="serv-show">
-                <li><a href="#">App Design</a></li>
-                <li><a href="#">Web Design</a></li>
-            </ul>
-        </li>
-        <li><a href="#">Portfolio</a></li>
-        <li><a href="#">Overview</a></li>
-        <li><a href="#">Shortcuts</a></li>
-        <li><a href="#">Feedback</a></li>
+<nav class="navbar">
+    <!-- LOGO -->
+    <div class="logo">MaryJane</div>
+    <!-- NAVIGATION MENU -->
+    <ul class="nav-links">
+        <!-- USING CHECKBOX HACK -->
+        <input type="checkbox" id="checkbox_toggle" />
+        <label for="checkbox_toggle" class="hamburger">&#9776;</label>
+
+        <!-- NAVIGATION MENUS -->
+        <div class="menu">
+            <li><a href="#">Home</a></li>
+            <li class="services">
+                <a href="">Seatworks</a>
+
+                <!-- DROPDOWN MENU -->
+                <ul class="dropdown">
+                    <li><a href="seatworks/ELECT2_SWM1.php">SWM1</a></li>
+                    <li><a href="seatworks/ELECT2_SWM2_1.php">SWM2.1</a></li>
+                    <li><a href="seatworks/ELECT2_SWM2_2.php">SWM2.1</a></li>
+                </ul>
+            </li>
+            <li class="services">
+                <a href="">Quizzes</a>
+
+                <!-- DROPDOWN MENU -->
+                <ul class="dropdown">
+                    <li><a href="#">Test1</a></li>
+                    <li><a href="#">Test1</a></li>
+                    <li><a href="#">Test1</a></li>
+                </ul>
+            </li>
+        </div>
     </ul>
 </nav>
-<div name="test1" class="content">
-    <div class="header">
-        Sidebar Menu with sub-menu
-    </div>
-    <p>
-        HTML CSS & Javascript (Full Tutorial)
-    </p>
-</div>
-<script>
-    $('.btn').click(function(){
-        $(this).toggleClass("click");
-        $('.sidebar').toggleClass("show");
-    });
-    $('.feat-btn').click(function(){
-        $('nav ul .feat-show').toggleClass("show");
-        $('nav ul .first').toggleClass("rotate");
-    });
-    $('.serv-btn').click(function(){
-        $('nav ul .serv-show').toggleClass("show1");
-        $('nav ul .second').toggleClass("rotate");
-    });
-    $('nav ul li').click(function(){
-        $(this).addClass("active").siblings().removeClass("active");
-    });
-</script>
 </body>
+
 </html>
