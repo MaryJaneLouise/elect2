6073ee2ab5883a0fa3cf49f3b8f49215e0187033
MaryJane <mj.louise007@gmail.com>
2023-02-14 23:55:43 +0800

    new navbar file reference

---
+++ navbar.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>NavigationPage</title>
+    <base target="pages">
+</head>
+
+<body>
+<table style="font-size:25px">
+    <tr>
+        <th>Navigation Page
+    </tr>
+    <tr>
+        <td><a href="intro_webpage.html" target="pagedisplay">Home Page</a>
+    </tr>
+    <td><a href="link_personal_info.html" target="pagedisplay">Personal Information</a>
+        </tr>
+        <tr>
+            <td><a href="link_academic_info.html" target="pagedisplay">Academic Information</a>
+        </tr>
+        <tr>
+            <td><a href="link_hobbiesskillstalents.html" target="pagedisplay">Hobbies/Skills/Talents</a>
+        </tr>
+        <tr>
+            <td><a href="link_serminars_attended.html" target="pagedisplay">Seminars Attended</a>
+        </tr>
+        <tr>
+            <td><a href="link_activities_attended.html" target="pagedisplay">Activities Attended/Conducted</a>
+        </tr>
+        <tr>
+            <td><a href="link_character_references.html" target="pagedisplay">Character Reference</a>
+        </tr>
+    </td>
+</table>
+</body>
+
+</html>
