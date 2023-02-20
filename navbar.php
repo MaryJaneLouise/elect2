bde9c3daea598583c40f7ebcfbc845c889b022b6
MaryJane <mj.louise007@gmail.com>
2023-02-21 00:01:13 +0800

    updated submenus

--- navbar.php
+++ navbar.php
@@ -9,8 +9,12 @@
         <a href="#">Lectures</a>
 
         <ul class="dropdown-menu">
-            <li><a href="#" onclick="changeFrame('lectures/Lecture_Simple.php')">Lecture 1</a></li>
-            <li><a href="#" onclick="changeFrame('lectures/Lecture_OOP.php')">Lecture 2</a></li>
+            <li><a href="#" onclick="changeFrame('lectures/Lecture_Simple.php')">Basics</a></li>
+            <li><a href="#" onclick="changeFrame('lectures/Lecture_OOP.php')">Basics (OOP)</a></li>
+            <li><a href="#" onclick="changeFrame('lectures/Lecture_Abstract.php')">OOP Abstract</a></li>
+            <li><a href="#" onclick="changeFrame('lectures/Lecture_Interfaces.php')">OOP Interfaces</a></li>
+            <li><a href="#" onclick="changeFrame('lectures/Lecture_Traits.php')">OOP Traits</a></li>
+            <li><a href="#" onclick="changeFrame('lectures/Lecture_CarEx.php')">OOP Form Example</a></li>
         </ul>
 
     </li>
@@ -22,6 +26,7 @@
             <li><a href="#" onclick="changeFrame('seatworks/ELECT2_SWM1.php')">SWM1</a></li>
             <li><a href="#" onclick="changeFrame('seatworks/ELECT2_SWM2_1.php')">SWM2.1</a></li>
             <li><a href="#" onclick="changeFrame('seatworks/ELECT2_SWM2_2.php')">SWM2.2</a></li>
+            <li><a href="#" onclick="changeFrame('seatworks/ELECT2_SWM2_2.php')">SWM3</a></li>
         </ul>
     </li>
     <li class="dropdown">
