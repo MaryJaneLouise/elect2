14f162ecea54a294b59c42fe232a6c1200aec511
MaryJane <mj.louise007@gmail.com>
2023-03-29 21:07:33 +0800

    changed title name

--- exam/midterm/ELECT2_ME37.php
+++ exam/midterm/ELECT2_ME37.php
@@ -41,7 +41,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
             crossorigin="anonymous"></script>
-    <title>ELECT2 SWM4</title>
+    <title>ELECT2 Midterm Exam 37</title>
 </head>
 
 <body>
